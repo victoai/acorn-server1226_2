8017a5e0e09d2908d4b93ee19a3b9917ada5963b
victoai <victoai999@gmail.com>
2024-12-26 10:13:32 +0900

    project

---
+++ src/main/java/com/acorn/acorn_server1226_2/HelloController.java
@@ -0,0 +1,15 @@
+package com.acorn.acorn_server1226_2;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/data")
+    public  String hi(){
+        return " hi DATA";
+    }
+}
